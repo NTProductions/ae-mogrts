0b1d7eb4d00430ecf285f4a1477d0b359d7a12ec
Nate Lovell <Natelovell99@gmail.com>
2021-02-02 01:05:36 -0700

    Create AE Mogrts.jsx

---
+++ AE Mogrts.jsx
@@ -0,0 +1,14 @@
+﻿// ae mogrts
+generateMogrt(app.project.activeItem.layer(1), app.project.activeItem);
+
+function generateMogrt(controllerLayer, comp) {
+    comp.openInEssentialGraphics();
+    for(var m = 1; m <= controllerLayer.Effects.numProperties; m++) {
+        controllerLayer.effect(m).property(1).addToMotionGraphicsTemplateAs(comp, controllerLayer.effect(m).name);
+        }
+    
+comp.motionGraphicsTemplateName = comp.name;
+
+comp.exportAsMotionGraphicsTemplate(true, "C:\\Users\\12082\\Desktop");
+
+    }
